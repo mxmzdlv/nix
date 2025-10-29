eca7bae1b9dbf1445220003ba3151e3cd4f80de0
mxmzdlv <maxim.zudilov@gmail.com>
2025-10-30 12:06:24 +1300

    wip

--- modules/common.nix
+++ modules/common.nix
@@ -1,25 +1,25 @@
 { pkgs, ... }:
 {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.settings.auto-optimise-store = true;
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 14d";
-  };
+  # nix.settings.auto-optimise-store = true;
+  # nix.gc = {
+  #   automatic = true;
+  #   dates = "weekly";
+  #   options = "--delete-older-than 14d";
+  # };
 
   # Cross-platform packages (keep it conservative here)
   environment.systemPackages = with pkgs; [
     git
-    gnupg
-    curl
-    wget
-    vim
-    htop
-    ripgrep
-    fd
-    jq
-    unzip
+    # gnupg
+    # curl
+    # wget
+    # vim
+    # htop
+    # ripgrep
+    # fd
+    # jq
+    # unzip
   ];
 
   programs.zsh.enable = true;
