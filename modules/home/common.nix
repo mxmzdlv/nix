df557b2f7c36ae871b704ac7fe078e4a29562b58
mxmzdlv <maxim.zudilov@gmail.com>
2025-11-01 12:37:36 +1300

    fix

--- modules/home/common.nix
+++ modules/home/common.nix
@@ -27,6 +27,44 @@ in
     pkgs.zed-editor
   ];
 
+
+  programs.dconf.enable = true;
+
+  dconf.settings = {
+    # Tiling with Super+Left/Right (GNOME Mutter)
+    "org/gnome/mutter/keybindings" = {
+      toggle-tiled-left = [];
+      toggle-tiled-right = [];
+    };
+
+    # Maximize/unmaximize on Super+Up/Down (GNOME WM)
+    "org/gnome/desktop/wm/keybindings" = {
+      maximize = [];
+      unmaximize = [];
+      # keep a non-super alternative for max/restore
+      toggle-maximized = ["<Alt>F10"];
+    };
+
+    # App switching with Super+1..9 (GNOME Shell)
+    "org/gnome/shell/keybindings" = {
+      switch-to-application-1 = [];
+      switch-to-application-2 = [];
+      switch-to-application-3 = [];
+      switch-to-application-4 = [];
+      switch-to-application-5 = [];
+      switch-to-application-6 = [];
+      switch-to-application-7 = [];
+      switch-to-application-8 = [];
+      switch-to-application-9 = [];
+    };
+
+    # If you run Dash-to-Dock, also kill its Super+number behavior
+    "org/gnome/shell/extensions/dash-to-dock" = {
+      hot-keys = false;
+    };
+  };
+
+
   # Shared application configuration synced into XDG config directory
   xdg.configFile = {
     "ghostty/config".text =
