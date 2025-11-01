5cfec6e6ea1406916d8cc7356e649640582286ad
mxmzdlv <maxim.zudilov@gmail.com>
2025-11-01 15:10:46 +1300

    Revert "fix"
    d49394d29bcc45566381de5bab93b3c37377f248

--- modules/home/common.nix
+++ modules/home/common.nix
@@ -24,46 +24,18 @@ in
   };
 
   home.packages = [
+    pkgs.walker
     pkgs.zed-editor
   ];
 
-
-  dconf.settings = {
-    # Tiling with Super+Left/Right (GNOME Mutter)
-    "org/gnome/mutter/keybindings" = {
-      toggle-tiled-left = [];
-      toggle-tiled-right = [];
-    };
-
-    # Maximize/unmaximize on Super+Up/Down (GNOME WM)
-    "org/gnome/desktop/wm/keybindings" = {
-      maximize = [];
-      unmaximize = [];
-      # keep a non-super alternative for max/restore
-      toggle-maximized = ["<Alt>F10"];
-    };
-
-    # App switching with Super+1..9 (GNOME Shell)
-    "org/gnome/shell/keybindings" = {
-      switch-to-application-1 = [];
-      switch-to-application-2 = [];
-      switch-to-application-3 = [];
-      switch-to-application-4 = [];
-      switch-to-application-5 = [];
-      switch-to-application-6 = [];
-      switch-to-application-7 = [];
-      switch-to-application-8 = [];
-      switch-to-application-9 = [];
-      # Disable overview on bare Super press so it can be remapped by the tiling WM
-      toggle-overview = [];
-    };
-
-    # If you run Dash-to-Dock, also kill its Super+number behavior
-    "org/gnome/shell/extensions/dash-to-dock" = {
-      hot-keys = false;
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      bind = [
+        "SUPER, RETURN, exec, ghostty"
+      ];
     };
   };
-
 
   # Shared application configuration synced into XDG config directory
   xdg.configFile = {
