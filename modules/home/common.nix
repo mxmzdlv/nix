7ea512eea46a9293620e909f2143278400a3c361
mxmzdlv <maxim.zudilov@gmail.com>
2025-11-01 15:56:06 +1300

    wip

--- modules/home/common.nix
+++ modules/home/common.nix
@@ -24,9 +24,46 @@ in
   };
 
   home.packages = [
-    pkgs.walker
     pkgs.zed-editor
   ];
+
+  dconf.settings = {
+    "org/gnome/mutter" = {
+      overlay-key = "";
+    };
+
+    # Tiling with Super+Left/Right (GNOME Mutter)
+    "org/gnome/mutter/keybindings" = {
+      toggle-tiled-left = [];
+      toggle-tiled-right = [];
+    };
+
+    # Maximize/unmaximize on Super+Up/Down (GNOME WM)
+    "org/gnome/desktop/wm/keybindings" = {
+      maximize = [];
+      unmaximize = [];
+      # keep a non-super alternative for max/restore
+      toggle-maximized = ["<Alt>F10"];
+      panel-main-menu = [];
+    };
+
+    # App switching with Super+1..9 (GNOME Shell)
+    "org/gnome/shell/keybindings" = {
+      switch-to-application-1 = [];
+      switch-to-application-2 = [];
+      switch-to-application-3 = [];
+      switch-to-application-4 = [];
+      switch-to-application-5 = [];
+      switch-to-application-6 = [];
+      switch-to-application-7 = [];
+      switch-to-application-8 = [];
+      switch-to-application-9 = [];
+      # Disable overview on bare Super press so it can be remapped by the tiling WM
+      toggle-overview = [ "" ];
+      toggle-application-view = [ "" ];
+    };
+  };
+
 
   # Shared application configuration synced into XDG config directory
   xdg.configFile = {
