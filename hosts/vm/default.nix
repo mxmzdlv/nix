cf84bf2ae4a13b569a9327346835d874c6bd6876
mxmzdlv <maxim.zudilov@gmail.com>
2025-11-01 22:08:07 +1300

    Revert "fix"
    fb2b5a2b1822b27f496123dc1c7c56277fe450fb

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -79,6 +79,10 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+    gnome-tweaks
+    gnome-themes-extra
+    gnome-user-share
+    gnomeExtensions.appindicator
     mesa-demos
     zed-editor
     ghostty
@@ -112,12 +116,20 @@
   system.stateVersion = "25.11"; # Did you read the comment?
 
   services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
+  services.displayManager.gdm.enable = true;
   services.displayManager.autoLogin = {
     enable = true;
     user = "maxim";
   };
-  services.desktopManager.plasma6.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  services.desktopManager.gnome.extraGSettingsOverridePackages = [ pkgs.mutter ];
+  services.desktopManager.gnome.extraGSettingsOverrides = ''
+    [org.gnome.mutter]
+    experimental-features=['scale-monitor-framebuffer', 'xwayland-native-scaling']
+    [org/gnome/desktop/interface]
+    scaling-factor=1.5
+  '';
 
   virtualisation.vmware.guest.enable = true;
 
