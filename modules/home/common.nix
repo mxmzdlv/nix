8c4d150166b6d29e1e5e9b2489307dda2807eef2
mxmzdlv <maxim.zudilov@gmail.com>
2025-12-04 23:11:27 +1300

    Auto-commit 2025-12-04T10:11:27Z

--- modules/home/common.nix
+++ modules/home/common.nix
@@ -265,6 +265,7 @@ in
       switch-to-application-9 = [];
       # Disable overview on bare Super press so it can be remapped by the tiling WM
       toggle-overview = [ "<Super>space" ];
+
     };
   };
 
