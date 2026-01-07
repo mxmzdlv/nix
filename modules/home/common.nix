34520efe4a37c1eca883954ceff1107bfaf8b10c
mxmzdlv <maxim.zudilov@gmail.com>
2026-01-08 12:43:12 +1300

    wip

--- modules/home/common.nix
+++ modules/home/common.nix
@@ -302,6 +302,11 @@ in
       n = ''
         zed ~/notes
       '';
+      develop = ''
+        function develop --wraps='nix develop'
+          env ANY_NIX_SHELL_PKGS=(basename (pwd))"#"(git describe --tags --dirty) (type -P nix) develop --command fish
+        end
+      '';
     };
     interactiveShellInit = ''
       if type -q opam
